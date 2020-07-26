8e72b9928ba121ece1935a42015f176345e68ca5
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-26 08:31:07 -0300

    api: editorconfig, eslint and prettier configuration

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
+  return res.json({ message: 'Hello World' });
 });
 
 app.listen(3333, () => {
-    console.log('Server started on http://localhost:3333');
+  console.log('Server started on http://localhost:3333');
 });
